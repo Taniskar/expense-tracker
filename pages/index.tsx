0ee405655a45914da28e2d88c794e9411daa7ee2
Hiren Patel <hirenpatel.ds@gmail.com>
2025-07-19 09:16:51 +0530

    Disable ESLint strict rules for deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+export default function Home() {
+  const router = useRouter();
 
+  useEffect(() => {
+    router.replace('/dashboard');
+  }, [router]);
+
+  return null;
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
